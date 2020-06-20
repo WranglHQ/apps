d4c359a511c51ab0fa3a27e18dd1ae3c9217cb35
Stuart Robinson <stuartrobinson@MacBook-Pro.attlocal.net>
2020-06-19 19:59:43 -0500

    wip publishing working?

--- src/mongo_lite.mjs
+++ src/mongo_lite.mjs
@@ -5,11 +5,17 @@ const API = 'https://d9k1wacmyi.execute-api.us-east-1.amazonaws.com'
 
 export async function storeFunctionToCloud({ content, token }) {
   try {
+    content = content.trim();
     const url = `${API}/dev/module`
     console.log('about to hit axios with token, url, content, config:');
+    console.log('token::');
     console.log(token);
+    console.log('url::');
     console.log(url);
+    console.log('content::');
     console.log(content);
+
+    console.log('----end of content----');
     //https://github.com/robisonmark/mhac/blob/4a511e24a3b6684dc1031e472bc47197868f06be/src/api/endpoints.js
     //https://stackoverflow.com/questions/6408904/send-post-request-with-data-specified-in-file-via-curl
     const config = { headers: { "Authorization": token, "Content-Type": "text/xml" } };
